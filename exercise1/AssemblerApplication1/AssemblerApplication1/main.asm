ac3a83f2a617f8afc4a57ee008b77a49eae814c8
Miska <michaela.golhova@gmail.com>
2018-10-08 10:16:46 +0200

    exercise4

--- exercise1/AssemblerApplication1/AssemblerApplication1/main.asm
+++ exercise1/AssemblerApplication1/AssemblerApplication1/main.asm
@@ -1,4 +1,5 @@
-ldi r16, 42   ; load decimal value into register r16
-ldi r17, 0x33 ; load hexadecimal value into register r16
-ldi r18, 0b10110010 ; load binary value into register r16
+ldi r17,  0x08 ; load hexadecimal value into register r17
+ldi r18, 0xb3 ; load hexadecimal value into register r18
+
+add r18, r17 ; add r17 to r18
 nop
