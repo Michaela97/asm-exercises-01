e913753d6bd51ed6ee9b8c85e8dca4564106f8ad
Miska <michaela.golhova@gmail.com>
2018-10-08 10:12:27 +0200

    exercise3

--- exercise1/AssemblerApplication1/AssemblerApplication1/main.asm
+++ exercise1/AssemblerApplication1/AssemblerApplication1/main.asm
@@ -1,2 +1,4 @@
-ldi r16, 0b0100_0010    ; load value into register r16
-nop                     ; does nothing, but provides a place to put a breakpoint
+ldi r16, 42   ; load decimal value into register r16
+ldi r17, 0x33 ; load hexadecimal value into register r16
+ldi r18, 0b10110010 ; load binary value into register r16
+nop
